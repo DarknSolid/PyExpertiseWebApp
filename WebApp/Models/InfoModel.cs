324535091401cec425d21821f55bd0c4dad05f38
Hampus Iversen <hampuswadum@hotmail.com>
2022-11-02 16:56:53 +0100

    added project

---
+++ WebApp/Models/InfoModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class InfoModel
+    {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [Url(ErrorMessage = "Invalid url")]
+            public string Url { get; set; }
+    }
+}
