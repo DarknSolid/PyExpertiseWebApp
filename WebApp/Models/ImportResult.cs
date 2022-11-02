7ef5beb210362bc314afb20bd55b89899f10f22a
Hampus Iversen <hampuswadum@hotmail.com>
2022-11-03 00:24:02 +0100

    added api integration

--- WebApp/Models/ImportResult.cs
+++ WebApp/Models/ImportResult.cs
@@ -2,6 +2,13 @@
 {
     public class ImportResult
     {
-        public IEnumerable<(string, int)> Imports { get; set; }
+        public IEnumerable<string> LibrariesToAssess { get; set; }
+        public IEnumerable<Import> Imports { get; set; }
+    }
+
+    public class Import
+    {
+        public string Library { get; set; }
+        public int Count { get; set; }
     }
 }
