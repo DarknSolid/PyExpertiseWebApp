80b3e536f13c187f10a7c3db7f2c1841ba352767
Hampus Iversen <hampuswadum@hotmail.com>
2022-11-16 09:34:27 +0100

    changed end result table. added back button on survey page

--- WebApp/Models/ImportResult.cs
+++ WebApp/Models/ImportResult.cs
@@ -3,6 +3,13 @@
     public class ImportResult
     {
         public IEnumerable<string> LibrariesToAssess { get; set; }
-        public IDictionary<string,int> Imports { get; set; }
+        public IEnumerable<Library> Imports { get; set; }
+    }
+
+    public class Library
+    {
+        public string Name { get; set; }
+        public int TotalImports { get; set; }
+        public bool IsStandardLibrary { get; set; }
     }
 }
