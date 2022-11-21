bbc1fe73a6ddfc97f1626a90a408e96cfb675555
Hampus Iversen <hampuswadum@hotmail.com>
2022-11-21 17:49:42 +0100

    finished additional comment - fixed api endpoint

---
+++ WebApp/Models/AdditionalSubmit.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApp.Models
+{
+    public class AdditionalSubmit
+    {
+        public string AdditionalComment { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+    }
+}
