5d605d919c7d674ad271995494a47e8c1a1653af
Hampus Iversen <hampuswadum@hotmail.com>
2022-11-21 11:42:17 +0100

    fixed table pagination, sorting and current library text

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -7,8 +7,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://PyExpertiseApi2.victorfalkensko.repl.co"), Timeout = TimeSpan.FromMinutes(60) });
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://127.0.0.1:5000/"), Timeout = TimeSpan.FromMinutes(10) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://PyExpertiseApi2.victorfalkensko.repl.co"), Timeout = TimeSpan.FromMinutes(60) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://127.0.0.1:5000/"), Timeout = TimeSpan.FromMinutes(10) });
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
